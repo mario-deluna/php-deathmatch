d30143e11631988b0cc8aff48e431c8727e425ed
Mario Döring <deluna.yy@gmail.com>
2018-05-30 09:25:44 +0200

    Added pistol + shader updates

--- resources/shader/base3d.frag.glsl
+++ resources/shader/base3d.frag.glsl
@@ -5,27 +5,48 @@ out vec4 fragment_color;
 in vec3 FragPos;  
 in vec3 FragNormals;
 in vec2 tCoords;
+in vec3 LightPos;
 
 uniform sampler2D texture1;
+uniform float time;
   
 void main()
 {
     vec4 textcol = texture(texture1, tCoords);
-    vec3 lightPos = vec3(13.0, 13.0, 13.0); 
     vec3 lightColor = vec3(1.0, 1.0, 0.9);
-    vec3 objectColor = vec3(0.7, 0.7, 0.7) * vec3(textcol.x, textcol.y, textcol.z);
-    vec3 cameraPos = vec3(0.0, 0.0, -10.0);
+    vec3 objectColor = vec3(0.2, 0.2, 0.2);
+    //vec3 objectColor = vec3(0.8, 0.8, 0.8);// * vec3(textcol.x, textcol.y, textcol.z);
     
     // ambient
-    float ambientStrength = 0.5;
-    vec3 ambient = ambientStrength * lightColor;
+    float ambientStrength = 0.6;
+    vec3 ambient = ambientStrength * lightColor;    
     
-    // diffuse 
+     // diffuse 
     vec3 norm = normalize(FragNormals);
-    vec3 lightDir = normalize(lightPos - FragPos * 0.1);
+    vec3 lightDir = normalize(LightPos - FragPos);
     float diff = max(dot(norm, lightDir), 0.0);
     vec3 diffuse = diff * lightColor;
-            
-    vec3 result = (ambient + diffuse) * objectColor;
+    
+    // specular
+    float specularStrength = 0.9;
+    vec3 viewDir = normalize(-FragPos); // the viewer is always at (0,0,0) in view-space, so viewDir is (0,0,0) - Position => -Position
+    vec3 reflectDir = reflect(-lightDir, norm);  
+    float spec = pow(max(dot(viewDir, reflectDir), 0.0), 32);
+    vec3 specular = specularStrength * spec * lightColor; 
+    
+    vec3 result = (ambient + diffuse + specular) * objectColor;
     fragment_color = vec4(result, 1.0);
+    
+    // // ambient
+    // float ambientStrength = 0.5;
+    // vec3 ambient = ambientStrength * lightColor;
+    
+    // // diffuse 
+    // vec3 norm = normalize(FragNormals);
+    // vec3 lightDir = normalize(lightPos - FragPos);
+    // float diff = max(dot(norm, lightDir), 0.0);
+    // vec3 diffuse = diff * lightColor;
+            
+    // vec3 result = (ambient + diffuse) * objectColor;
+    // fragment_color = vec4(result, 1.0);
 } 
--- resources/shader/base3d.vert.glsl
+++ resources/shader/base3d.vert.glsl
@@ -10,11 +10,21 @@ uniform mat4 projection;
 out vec3 FragPos;
 out vec3 FragNormals;
 out vec2 tCoords;
+out vec3 LightPos;
 
 void main()
 {
-    gl_Position = projection * view * transform * vec4(position, 1.0f);
-    FragPos = vec3(transform * vec4(position, 1.0));
-    FragNormals = mat3(transpose(inverse(transform))) * normalv;
+	vec3 lightPos = vec3(10, 10, 10);
+
+	gl_Position = projection * view * transform * vec4(position, 1.0);
+    FragPos = vec3(view * transform * vec4(position, 1.0));
+    FragNormals = mat3(transpose(inverse(view * transform))) * normalv;
+    LightPos = vec3(view * vec4(lightPos, 1.0));
     tCoords = text_coords;
+
+
+    // gl_Position = projection * view * transform * vec4(position, 1.0f);
+    // FragPos = vec3(transform * vec4(position, 1.0));
+    // FragNormals = mat3(transpose(inverse(transform))) * normalv;
+    // tCoords = text_coords;
 }
